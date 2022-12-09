1984f3de682ca3e6963afb2cd3fa24b011d08bb7
Heebrah <ibrahimola08@gmail.com>
2022-12-09 03:12:14 -0800

    4th task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/**
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+	char str[] = "\"Programming is like building a multilingual puzzle";
+{
+	puts(str);
+	return (0);
+}
