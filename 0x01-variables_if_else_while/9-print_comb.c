6af2d8aba0d8c26173fcc0c2fb81917532345b76
Heebrah <ibrahimola08@gmail.com>
2022-12-12 15:39:27 -0800

    9th

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,11 @@ int main(void)
 {
 	int n;
 
-	flor (n = 48; n < 58; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
-		if (n != 57)<F12>
+	}
+		if (n != 57)
 		{
 			putchar(',');
 			putchar(' ');
