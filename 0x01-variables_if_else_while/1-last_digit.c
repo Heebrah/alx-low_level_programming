e690389128ddffad9e60ffe1a7530cbab344b7b9
Heebrah <ibrahimola08@gmail.com>
2022-12-12 03:19:05 -0800

    0 task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,11 +13,17 @@ int main(void)
 srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (number > 0)
-		printf("number of %d is %d and is positive\n", n, number);
-	else if (number = 0)
-		printf("number of %d is %d and is zero\n", n, number)
-	else
-		printf("number of %d is %d and is negative\n", n, number)
-		return (0);
+	{
+		printf("is positiv\n", n, number);
+	}
+		else if (number = 0)
+	{
+		printf("is zero\n", n, number)
+	}
+				else
+	{
+		printf("is negative\n", n, number)
+	}
+			return (0);
 
 }
