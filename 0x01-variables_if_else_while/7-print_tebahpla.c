9250495c8052df12cfb72c4b595c8192afeef957
Heebrah <ibrahimola08@gmail.com>
2022-12-12 15:16:22 -0800

    7task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabets in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char sl;
+
+	for (sl = 'z'; sl >= 'a'; sl--)
+		putchar(sl)
+
+			putchar('\n');
+
+	return (0);
+}
