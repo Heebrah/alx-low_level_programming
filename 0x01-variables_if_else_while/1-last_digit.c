e067f7f29437c57f828520b031400aa42058f572
Heebrah <ibrahimola08@gmail.com>
2022-12-12 13:17:27 -0800

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry
+ * main - function
  * Return: Always 0
  *
  */
 int main(void)
+
 {
 	int n;
 	int b;
 
-	strand(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	b = n % 10;
 
