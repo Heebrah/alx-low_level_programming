5819a74015317968ab1282bb3e6dd3b0813e6f08
Heebrah <ibrahimola08@gmail.com>
2022-12-12 13:47:38 -0800

    2nd task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints alpha in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char h;
+	for (h = 'a;' h <= 'z'; h++)
+{
+		putchar(h);
+	}
+	putchar('\n');
+	return (0);
+}
