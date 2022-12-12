faeedaa874e0e4081bc54425ffd8f9675c98fbc0
Heebrah <ibrahimola08@gmail.com>
2022-12-12 15:10:03 -0800

    task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,11 +2,12 @@
 
 /**
  * main - prints single digits in base 10
- * Retrun: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
 	int num;
+
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
