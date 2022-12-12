738045af7a6a1f856c647392df2cc596797888a1
Heebrah <ibrahimola08@gmail.com>
2022-12-12 02:12:01 -0800

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,18 +5,18 @@
  *
  * Return: Always 0 (sucess)
  */
+
 int main(void)
 {
 	char c;
-	int l;
+	int i;
 	long int l;
 	long long int ll;
 	float f;
-
 	printf("size of a char: %lu byte(s)\n", (unsigned long) sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", (signed long) sizeof(lon long int));
+	printf("Size of a long long int: %lu byte(s)\n", (signed long) sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
 	return (0);
 }
