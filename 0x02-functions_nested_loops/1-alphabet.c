1a50afddfbc70e5a168626e9afda78efd9f8f5ee
Heebrah <ibrahimola08@gmail.com>
2022-12-13 13:56:40 -0800

    1 task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet in lower case
+ */
+void print_alphabet(void)
+{
+	char lc;
+	for (lc = 'a'; lc <= 'z'; lc++)
+		_putchar (lc);
+	_putchar ('\n');
+
+	return (0)
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints char 10 times
+ */
+void print_alphabet_x10(void);
+{
+	int num
+		char lc
+		for (num = 0; num > 10; num++)
+		{
+			for (lc = 'a'; lc <= 'z'; lc++)
+				_putchar(lc);
+			_putchar('\n');
+		}
+}
