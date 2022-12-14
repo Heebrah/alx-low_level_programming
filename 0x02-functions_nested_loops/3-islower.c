01e8827ca4b0794f0daee264d10330fbfb07cbc5
Heebrah <ibrahimola08@gmail.com>
2022-12-14 08:05:33 -0800

    3rd task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - function to check lowercase
+ * @c: parameter
+ * Return: allow success
+ */
+int _islower(int c);
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
