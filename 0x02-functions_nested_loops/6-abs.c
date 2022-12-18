426f4c80f70780fc634cae0807d6b5d4dccda0f2
Heebrah <ibrahimola08@gmail.com>
2022-12-18 02:50:34 -0800

    absolute task

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,12 +2,14 @@
 /**
  * _abs - function that shows the absolute value
  * @n: parameter of function
+ * Return: allow success
  */
 int _abs(int n)
 {
 	int a;
 
 	a = -1 * n;
+
 	if (n > 0)
 		return (n);
 	else
