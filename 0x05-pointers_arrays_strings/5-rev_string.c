3ac39c7958105c23cde0a5fb7a857db6caad214f
Heebrah <ibrahimola08@gmail.com>
2022-12-20 13:28:24 -0800

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,9 +15,9 @@ line2 = 0;
 	while (s[line1] != '\0')
 		line1++;
 
-		line2 = line1 - 1;
-		for (i = 0; i < line1 / 2; i++)
-		{
+	line2 = line1 - 1;
+	for (i = 0; i < line1 / 2; i++)
+	{
 			tmp = s[i];
 			s[i] = s[line2];
 			s[line2] = tmp;
