6e6473d50e79f4ecb56c8f65138f0133bd849e99
Heebrah <ibrahimola08@gmail.com>
2023-01-03 06:15:26 -0800

    debug 0 task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - tests function that prints if int is +ve or _ve
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
