f8a492f4336bf6ddfb51d79ed299ffe24f0e10c4
Heebrah <ibrahimola08@gmail.com>
2023-01-05 03:08:50 -0800

    print no

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - prints number followed by a line
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c >= '0'; c <= '9'; c++)
+	{
+		_putchar (c);
+	}
+	_putchar ('\n')
+}
+
+
+
