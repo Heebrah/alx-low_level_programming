a763f4134d39961ec39921d5d78652fd0a13608b
Heebrah <ibrahimola08@gmail.com>
2023-01-18 03:48:20 -0800

    reverse array of integers

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -13,10 +13,10 @@ void print_array(int *a, int n)
 	i = 0;
 	while (i < n)
 	{
-	if (i != 0)
-	{
-		printf(", ");
-	}
+		if (i != 0)
+		{
+			printf(", ");
+		}
 		printf("%d", a[i]);
 		i++;
 	}
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,4 +5,18 @@
  * @n:integer
  * Return:void
  */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
 
+	i = 0;
+	n = n - 1;
+	while (i < n)
+	{
+		temp = *(a + i);
+		*(a + i) = *(a + n);
+		*(a + n) = temp;
+		i++;
+		n--;
+	}
+}
